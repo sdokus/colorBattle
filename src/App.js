d5ae4c321d5d7e8c5e05b478f7068b38591a6202
sdokus <sami2464@gmail.com>
2022-05-12 11:57:34 -0400

    fixed bug with api call - adjusted how color hex was being passed through variables, which seemed to be creating a reference error.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const colorUrl = "https://unpkg.com/color-name-list@9.6.0/dist/colornames.json";
 
 function App() {
+  const [colorData, setColorData] = useState({});
+
+  useEffect(() => {
+    getColorWithFetch();
+  }, []);
+
+  const getColorWithFetch = async () => {
+    const response = await fetch(colorUrl);
+    const jsonData = await response.json();
+    setColorData(jsonData);
+  };
+
+  let color1Index = Math.floor(Math.random() * colorData.length);
+  // let color1Hex = colorData[color1Index].hex;
+  let color2Index = Math.floor(Math.random() * colorData.length);
+  // let color2Hex = colorData[color2Index].hex;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>What's Your Favorite Color, Really?</h2>
       </header>
+      <div className="color-container">
+        <h3
+          className="color-item"
+          style={{ backgroundColor: colorData[color1Index].hex }}
+        >
+          {colorData[color1Index].name}
+        </h3>
+        <h2>VS.</h2>
+        <h3
+          className="color-item"
+          style={{ backgroundColor: colorData[color2Index].hex }}
+        >
+          {colorData[color2Index].name}
+        </h3>
+      </div>
     </div>
   );
 }
